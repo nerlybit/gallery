0dcd9789f79e1b8a1f4070f825979450fe6872e4
Nerly Biton <nbiton@yotpo.com>
2021-02-17 17:09:51 +0200

    no message

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,23 @@
 Rails.application.routes.draw do
+
+
+  # get 'image/new'
+  # get 'image/create'
+  # get 'image/destroy'
+  # post 'image/create'
+
+
+  root 'albums#index'
+  get 'images/new'
+
+  resources :albums do
+    resources :images
+  end
+
+  post '/album/:id/images/:id' , to: 'image#new'
+  post '/album/1/image' , to: 'image#new'
+  post '/image' , to: 'image#new'
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
